69a27f6f25b6d38a2cacc7c82347d08e6bb5a140
modouaicha023 <modouaichadiop@gmail.com>
2025-01-18 12:05:44 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import { ToggleTheme } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  const res = await fetch(
-    process.env.NEXT_PUBLIC_BASE_API_URL + "/users/contributions/senegal"
-  );
-  const data = await res.json();
+  // const res = await fetch(
+  //   process.env.NEXT_PUBLIC_BASE_API_URL + "/users/contributions/senegal"
+  // );
+  // const data = await res.json();
   return (
     <div>
       <ToggleTheme />
