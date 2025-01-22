bf0a3f9d3bba2c6f2473ec10b805526c6e233a19
modouaicha023 <modouaichadiop@gmail.com>
2025-01-22 22:49:08 +0000

    phewww :) update export

--- components/statistics.tsx
+++ components/statistics.tsx
@@ -8,11 +8,13 @@ export const Statistics = ({ data }: StatisticsProps) => {
       {data.map(({ quantity, description, subtitle }) => (
         <div
           key={description}
-          className="text-center w-fit flex gap-4 items-center justify-center"
+          className="text-center w-fit flex gap-2 items-center justify-center"
         >
-          <Icons.gihub2 />
-          +<CountAnimation number={quantity} className="text-4xl w-4" />
-          <p className="text-sm ml-2 w-8"> {description}</p>
+          <Icons.gihub2 className="" />
+          <div className="flex flex-col gap-4 items-center">
+            <CountAnimation number={quantity} className="text-4xl w-32" />
+            <p className="text-sm ml-2 w-32"> {description}</p>
+          </div>
           {/* <p className="text-xs text-muted-foreground  hidden line-clamp-1">
             {subtitle}
           </p> */}
