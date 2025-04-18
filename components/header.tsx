5f6427382b7e6391b315d9f0b14c6d8c6ba0351c
nijuse <xeesujin@gmail.com>
2025-04-18 15:09:09 +0900

    fix: theme toggle bug

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { ToggleTheme } from "./theme-toggle";
 
 const Header = () => {
   return (
-    <header className="w-full h-fit  flex justify-between items-center content-center">
+    <header className="border border-red w-full h-fit px-2 py-2 flex justify-between items-center content-center">
       <ToggleTheme />
     </header>
   );
